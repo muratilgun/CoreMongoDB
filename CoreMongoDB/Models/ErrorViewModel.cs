8adf12991808ea9b562e514ab590a98573d86b9c
muratilgun <muratilgun34@gmail.com>
2022-01-13 20:17:55 +0300

    Initial commit

---
+++ CoreMongoDB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreMongoDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
