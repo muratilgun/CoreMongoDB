cdc2650169dfc694e646af6fb98b195afd512f7e
muratilgun <muratilgun34@gmail.com>
2022-01-13 21:44:43 +0300

    Create IEmployeeRepository.cs

---
+++ CoreMongoDB/IRepository/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreMongoDB.Models;
+
+namespace CoreMongoDB.IRepository
+{
+    public interface IEmployeeRepository
+    {
+        Employee Save(Employee employee);
+        Employee Get(string employeeId);
+        List<Employee> Gets();
+        string Delete(string employeeId);
+    }
+}
