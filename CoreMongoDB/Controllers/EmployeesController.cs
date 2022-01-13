a0fe8843ccaf47810f24b4aa219b072beae53171
muratilgun <muratilgun34@gmail.com>
2022-01-13 22:07:51 +0300

    EmployeesController

---
+++ CoreMongoDB/Controllers/EmployeesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreMongoDB.IRepository;
+using CoreMongoDB.Models;
+
+namespace CoreMongoDB.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private IEmployeeRepository _employeeRepository = null;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEmployees()
+        {
+            var employees = _employeeRepository.Gets();
+            return Json(employees);
+        }
+
+        public JsonResult Save(Employee employee)
+        {
+            var employees = _employeeRepository.Save(employee);
+            return Json(employees);
+        }
+        public JsonResult DeleteEmployee(string employeeId)
+        {
+            string message = _employeeRepository.Delete(employeeId);
+            return Json(message);
+        }
+
+    }
+}
