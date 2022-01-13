a5f1af464d448a7f6d5d5abf46390adcb7fb2e5c
muratilgun <muratilgun34@gmail.com>
2022-01-13 22:01:38 +0300

    Implemented EmployeeRepository

---
+++ CoreMongoDB/Repository/EmployeeRepository.cs
@@ -0,0 +1,55 @@
+﻿using CoreMongoDB.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreMongoDB.Models;
+using MongoDB.Driver;
+
+namespace CoreMongoDB.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private MongoClient _mongoClient = null;
+        private IMongoDatabase _database = null;
+        private IMongoCollection<Employee> _employeeTable = null;
+
+        public EmployeeRepository()
+        {
+            _mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+            _database = _mongoClient.GetDatabase("OfficeDB");
+            _employeeTable = _database.GetCollection<Employee>("Employees");
+        }
+        public Employee Save(Employee employee)
+        {
+            var empObj = _employeeTable.Find(x => x.Id == employee.Id).FirstOrDefault();
+            if (empObj == null)
+            {
+                _employeeTable.InsertOne(employee);
+            }
+            else
+            {
+                _employeeTable.ReplaceOne(x => x.Id == employee.Id, employee);
+            }
+
+            return employee;
+        }
+        
+
+        public Employee Get(string employeeId)
+        {
+            return _employeeTable.Find(x => x.Id == employeeId).FirstOrDefault();
+        }
+
+        public List<Employee> Gets()
+        {
+            return _employeeTable.Find(FilterDefinition<Employee>.Empty).ToList();
+        }
+
+        public string Delete(string employeeId)
+        {
+            _employeeTable.DeleteOne(x => x.Id == employeeId);
+            return "Deleted";
+        }
+    }
+}
