e17730d050669b407392cfe1c926446099b1fcca
muratilgun <muratilgun34@gmail.com>
2022-02-13 20:51:44 +0300

    Added new project

---
+++ ImageSaveAndReadCoreMongoDB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImageSaveAndReadCoreMongoDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
