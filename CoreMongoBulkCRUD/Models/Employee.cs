788bfa92b87b0974f642714bd834b7f8303695c1
muratilgun <muratilgun34@gmail.com>
2022-02-25 23:19:23 +0300

    Created New Project for Bulk Ops

---
+++ CoreMongoBulkCRUD/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ImageSaveAndReadCoreMongoDB.Models
+{
+    public class Employee
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+    }
+}
---
+++ CoreMongoBulkCRUD/Repository/EmployeeRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreMongoBulkCRUD.IRepository;
+using ImageSaveAndReadCoreMongoDB.Models;
+using MongoDB.Driver;
+
+namespace CoreMongoBulkCRUD.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private MongoClient _mongoClient = null;
+        private IMongoDatabase _database = null;
+        private IMongoCollection<Employee> _empTable = null;
+
+        public EmployeeRepository()
+        {
+            _mongoClient = new MongoClient("mongodb://127.0.0.1:27017/");
+            _database = _mongoClient.GetDatabase("OfficeDB");
+            _empTable = _database.GetCollection<Employee>("Employee");
+        }
+
+        public EmployeeRepository(MongoClient mongoClient)
+        {
+            _mongoClient = mongoClient;
+        }
+
+        public void BulkSave()
+        {
+            var emps = new List<Employee>();
+            for (int i = 1; i < 9; i++)
+            {
+                emps.Add(new Employee()
+                {
+                    Name = "Emp"+i,
+                    Age = this.GetRandomAge()
+                });
+            }
+            _empTable.InsertMany(emps);
+        }
+
+        private int GetRandomAge()
+        {
+            var ages = new[] { 20, 21, 22, 23 };
+            Random random = new Random();
+            return ages[random.Next(ages.Length)];
+        }
+
+        public void BulkUpdate()
+        {
+            var emps = _empTable.Find(FilterDefinition<Employee>.Empty).ToList();
+            var filter = Builders<Employee>.Filter.In(x => x.Id, emps.Select(x => x.Id));
+            var update = Builders<Employee>.Update.Set(x=> x.Age,30);
+            _empTable.UpdateMany(filter, update);
+        }
+
+        public void BulkDeleteByPropertyValue()
+        {
+            _empTable.DeleteMany(x=> x.Age == 20);
+        }
+
+        public void BulkDelete()
+        {
+            var emps = _empTable.Find(FilterDefinition<Employee>.Empty).ToList();
+            emps.RemoveAt(emps.Count -1);
+            var filter = Builders<Employee>.Filter.In(x => x.Id, emps.Select(x => x.Id));
+            _empTable.DeleteMany(filter);
+        }
+    }
+}
